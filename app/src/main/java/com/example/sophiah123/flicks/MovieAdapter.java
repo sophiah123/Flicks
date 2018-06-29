e0749c2c8803f4c439d5b2b8e5513ea5cb40b960
Sophia Hu <sophiahu@fb.com>
2018-06-29 15:51:44 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sophiah123/flicks/MovieAdapter.java
@@ -0,0 +1,100 @@
+package com.example.sophiah123.flicks;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.sophiah123.flicks.models.Config;
+import com.example.sophiah123.flicks.models.Movie;
+
+import java.util.ArrayList;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+    //list of movies
+    ArrayList<Movie> movies;
+    //config needed for image urls
+    Config config;
+    //Context for rendering
+    Context context;
+
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    //creates and inflates a new view
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        //get the context and create the inflater
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //create the view using the using the item_movie layout
+        View movieView= inflater.inflate(R.layout.item_movie, parent, false);
+        //return a new ViewHolder
+        return new ViewHolder(movieView);
+    }
+
+    //binds an inflated view to a new item
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    // get the movie data at the specified position
+        Movie movie = movies.get(position);
+        //populate the view with the movie data
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+
+        //build url for poster image
+        String imageUrl= config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+
+        //load image using Glide
+        Glide.with(context)
+                .load(imageUrl)
+                .apply(new RequestOptions()
+                    .placeholder(R.drawable.flicks_movie_placeholder)
+                        .error(R.drawable.flicks_movie_placeholder)
+                        .transform(new RoundedCornersTransformation(25, 10)))
+                .into(holder.ivPosterImage);
+        //.apply(new RequestOptions().bitmapTransform(new RoundedCornersTransformation(context, 25, 0)))
+
+    }
+
+    //returns the total number of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    //create the viewholder as a static inner class
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        //track view objects
+
+        ImageView ivPosterImage;
+        TextView tvTitle;
+        TextView tvOverview;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //lookup view objects by id
+            ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
+            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+        }
+    }
+}
