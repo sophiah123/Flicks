daf1399fc420eeffc4a6341fa1d5c48f2d376ef7
Sophia Hu <sophiahu@fb.com>
2018-06-29 17:27:33 -0700

    Orientation changes

--- app/src/main/java/com/example/sophiah123/flicks/MovieAdapter.java
+++ app/src/main/java/com/example/sophiah123/flicks/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.sophiah123.flicks;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -59,17 +60,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         holder.tvTitle.setText(movie.getTitle());
         holder.tvOverview.setText(movie.getOverview());
 
+        //determine the current orientation
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
         //build url for poster image
-        String imageUrl= config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        String imageUrl= null;
+
+        //if in portrait mode, load the poster image
+        if (isPortrait){
+            imageUrl= config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else{
+            //load the backdrop image if the orientation is landscape
+            imageUrl= config.getImageUrl(config.getBackdropSize(), movie.getBackdropPath());
+        }
+
+        //get the correct placeholder and imageview for the current orientation
+        int placeholderId=isPortrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? holder.ivPosterImage : holder.ivBackdropImage;
 
         //load image using Glide
         Glide.with(context)
                 .load(imageUrl)
                 .apply(new RequestOptions()
-                    .placeholder(R.drawable.flicks_movie_placeholder)
-                        .error(R.drawable.flicks_movie_placeholder)
+                    .placeholder(placeholderId)
+                        .error(placeholderId)
                         .transform(new RoundedCornersTransformation(25, 10)))
-                .into(holder.ivPosterImage);
+                .into(imageView);
         //.apply(new RequestOptions().bitmapTransform(new RoundedCornersTransformation(context, 25, 0)))
 
     }
@@ -86,6 +102,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         //track view objects
 
         ImageView ivPosterImage;
+        ImageView ivBackdropImage;
         TextView tvTitle;
         TextView tvOverview;
 
@@ -93,6 +110,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             super(itemView);
             //lookup view objects by id
             ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
+            ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
             tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
             tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
         }
--- app/src/main/java/com/example/sophiah123/flicks/models/Config.java
+++ app/src/main/java/com/example/sophiah123/flicks/models/Config.java
@@ -9,27 +9,37 @@ public class Config {
     String imageBaseUrl;
     //the poster size to use when fetching images, part of the url
     String posterSize;
+    //the baackdrop size to use when fetching images
+    String backdropSize;
 
     public Config(JSONObject object) throws JSONException {
         JSONObject images = object.getJSONObject("images");
         //get the image base url
         imageBaseUrl = images.getString("secure_base_url");
         //get the poster size
-        JSONArray posterSizeOptions=images.getJSONArray("poster_sizes");
+        JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
         //use the option at index 3 or w342 as a fallback
-        posterSize=posterSizeOptions.optString(3, "w342");
+        posterSize = posterSizeOptions.optString(3, "w342");
+        //parse the backdrop sizes and use the option at index 1 or w780 as a fallback
+        JSONArray backdropSizeOptions = images.getJSONArray("backdrop_sizes");
+        backdropSize = backdropSizeOptions.optString(1, "w780");
+
     }
 
-    //helper method for creating urls
-    public String getImageUrl(String size, String path) {
-        return String.format("%s%s%s", imageBaseUrl, size, path); //concatenate all three
-    }
+        public String getImageUrl (String size, String path){
+            return String.format("%s%s%s", imageBaseUrl, size, path); //concatenate all three
+        }
 
-        public String getImageBaseUrl() {
-        return imageBaseUrl;
-    }
+        public String getImageBaseUrl () {
+            return imageBaseUrl;
+        }
 
-    public String getPosterSize() {
-        return posterSize;
+        public String getPosterSize () {
+            return posterSize;
+        }
+
+
+    public String getBackdropSize() {
+        return backdropSize;
     }
 }
